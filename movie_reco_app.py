4d44da1a158171ed8e124723222352f0a59396b3
Michal Tesler <michal.tesler@gmail.com>
2023-03-01 16:54:28 +0100

    Merge two initial files to one dataset

--- movie_reco_app.py
+++ movie_reco_app.py
@@ -1,4 +1,9 @@
 import pandas as pd
 
+# import movie dataset
 credits_df = pd.read_csv('tmdb_5000_credits.csv')
 movies_df = pd.read_csv('tmdb_5000_movies.csv')
+
+# combine two files
+credits_df.columns = ['id', 'title', 'cast', 'crew']
+movies_df = movies_df.merge(credits_df, on='id')
